50ac76b32e2a0185aed005007bd54f70b6e7ce2e
cleverapollo <cleverapollo@gmail.com>
2022-10-17 13:41:49 +0200

    small refactoring

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -31,39 +31,7 @@ import { Table as MuiTable, TableContainer } from "@mui/material";
 import TableHeader from "./TableHeader";
 import TableToolbar from "./TableToolbar";
 import TableBody from './TableBody';
-import { calculateColumnsWithPermissions, createTitleForProfileTypes } from "../../utils/table";
-
-declare module '@tanstack/table-core' {
-  interface FilterFns {
-    fuzzy: FilterFn<unknown>
-  }
-  interface FilterMeta {
-    itemRank: RankingInfo
-  }
-}
-
-const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
-  const itemRank = rankItem(row.getValue(columnId), value);
-
-  addMeta({
-    itemRank,
-  });
-
-  return itemRank.passed;
-}
-
-const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
-  let dir = 0;
-
-  if (rowA.columnFiltersMeta[columnId]) {
-    dir = compareItems(
-      rowA.columnFiltersMeta[columnId]?.itemRank!,
-      rowB.columnFiltersMeta[columnId]?.itemRank!
-    );
-  }
-
-  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir;
-}
+import { calculateColumnsWithPermissions, createTitleForProfileTypes, fuzzyFilter, fuzzySort } from "../../utils/table";
 
 const Table = <TData,>(props: ITableProps<TData>) => {
   const { data, title, titleOverride, columns } = props;
--- src/utils/table.ts
+++ src/utils/table.ts
@@ -1,3 +1,5 @@
+import { compareItems, RankingInfo, rankItem } from "@tanstack/match-sorter-utils";
+import { sortingFns, FilterFn, SortingFn } from '@tanstack/react-table';
 import { TableColumn, TitleOverride } from "../components/table/types";
 
 export function calculateColumnsWithPermissions<TData> (
@@ -40,4 +42,36 @@ export const createFormattedStringForDayjs = (value: string): string => {
     return value;
   }
   return `${arr[1]}.${arr[0]}.${arr[2]}`;
+}
+
+declare module '@tanstack/table-core' {
+  interface FilterFns {
+    fuzzy: FilterFn<unknown>
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo
+  }
+}
+
+export const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  const itemRank = rankItem(row.getValue(columnId), value);
+
+  addMeta({
+    itemRank,
+  });
+
+  return itemRank.passed;
+}
+
+export const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
+  let dir = 0;
+
+  if (rowA.columnFiltersMeta[columnId]) {
+    dir = compareItems(
+      rowA.columnFiltersMeta[columnId]?.itemRank!,
+      rowB.columnFiltersMeta[columnId]?.itemRank!
+    );
+  }
+
+  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir;
 }
