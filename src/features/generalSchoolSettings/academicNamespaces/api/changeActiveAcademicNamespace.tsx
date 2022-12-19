afa106b7fe9991fa4f2c8464f1a6f3a736768748
cleverapollo <cleverapollo@gmail.com>
2022-12-19 11:28:56 +0000

    pr comment changes

--- src/features/generalSchoolSettings/academicNamespaces/api/changeActiveAcademicNamespace.tsx
+++ src/features/generalSchoolSettings/academicNamespaces/api/changeActiveAcademicNamespace.tsx
@@ -15,10 +15,22 @@ const core_setActiveActiveAcademicNamespace = graphql(/* GraphQL */ `
     }
 `);
 
+export interface CoreSetActiveActiveAcademicNamespaceWrapper {
+    mutationBody: SetActiveAcademicNamespace,
+    displayChangeToYear: number
+}
 export function useCoreSetActiveActiveAcademicNamespace() {
+    const queryClient = useQueryClient();
 
     return useMutation({
         mutationKey: ['coreAcademicNamespace', 'core_setActiveActiveAcademicNamespace'],
-        mutationFn: async (input: SetActiveAcademicNamespace) => gqlClient.request(core_setActiveActiveAcademicNamespace, { input: input }),
+        mutationFn: async (input: CoreSetActiveActiveAcademicNamespaceWrapper) => gqlClient.request(core_setActiveActiveAcademicNamespace, { input: input.mutationBody }),
+        onSuccess: (a, b) => {
+            console.log(`Successfully changed year to ${b.displayChangeToYear}`)
+            queryClient.clear()
+        },
+        onError: (a, b) => {
+            console.log(`Failed changing year to ${b.displayChangeToYear}`)
+        }
     });
 }
