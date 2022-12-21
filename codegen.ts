1460616f6bd47bdf2419e69fdbc07520f208ba26
cleverapollo <cleverapollo@gmail.com>
2022-12-21 16:16:15 +0000

    code gen reverted

--- codegen.ts
+++ codegen.ts
@@ -7,7 +7,7 @@ if (!process.env.token) {
 const config: CodegenConfig = {
   schema: [
     {
-        'http://localhost/api/graphql': {
+        'https://tyro-api-uat.azurewebsites.net/api/graphql': {
         headers: {
           Authorization: `Bearer ${process.env.token}`,
         },
