3a8f67ed6f3f28a7183f9b167cc3b46f11c082ec
cleverapollo <cleverapollo@gmail.com>
2023-01-09 12:00:15 +0000

    fix missed typescript issue

--- src/features/generalSchoolSettings/rooms/pages/index.tsx
+++ src/features/generalSchoolSettings/rooms/pages/index.tsx
@@ -1,31 +1,30 @@
 import Table from '../../../../components/table/Table';
 import { TableColumn } from '../../../../components/table/types';
 import {  Container, Typography } from "@mui/material";
-import { useTranslation } from '@tyro/i18n';
+import { useTranslation, TFunction } from '@tyro/i18n';
 import Page from "../../../../components/Page";
 import useSettings from "../../../../hooks/useSettings";
 import {useCoreRooms} from "../api/rooms";
 import { useMemo } from 'react';
 import {Room} from "@tyro/api";
-import { TFunction } from 'i18next';
 
 
-const getSubjectColumns = (translate: TFunction): TableColumn<Room>[] => ([
+const getSubjectColumns = (translate: TFunction<("authentication" | "common")[], undefined, ("authentication" | "common")[]>): TableColumn<Room>[] => ([
   {
-    columnDisplayName: translate('Name'),
+    columnDisplayName: translate('authentication:name'),
     fieldName: 'name',
     filter: 'suggest',
     isMandatory: true,
   },
   {
-    columnDisplayName: translate('Capacity'),
+    columnDisplayName: translate('common:capacity'),
     fieldName: 'capacity',
     filter: 'suggest',
   },
 ]);
 
 export function Rooms() {
-  const { t } = useTranslation();
+  const { t } = useTranslation(['common', 'authentication']);
   const { themeStretch } = useSettings();
   const { data, isLoading } = useCoreRooms();
 
