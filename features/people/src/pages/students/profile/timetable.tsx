d48cf73c9de59c3891347735a9aa8bf881f15e61
cleverapollo <cleverapollo@gmail.com>
2023-03-03 13:46:25 +0000

    useNumber hook added instead of cast

--- features/people/src/pages/students/profile/timetable.tsx
+++ features/people/src/pages/students/profile/timetable.tsx
@@ -1,13 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { Container } from '@mui/material';
 import { Calendar } from '@tyro/calendar';
+import { useNumber } from '@tyro/core';
 
 export default function StudentProfileTimetablePage() {
   const { id } = useParams();
-
+  const studentId = useNumber(id);
   return (
     <Container maxWidth="xl">
-      <Calendar partyId={id as number | undefined} />
+      <Calendar partyId={studentId} />
     </Container>
   );
 }
