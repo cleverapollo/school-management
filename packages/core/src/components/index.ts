559254fd3cc6ed3ad1598580c0b5253e9d7c6582
cleverapollo <cleverapollo@gmail.com>
2023-03-08 15:31:06 +0000

    add CopyClipboardButton component

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -3,6 +3,7 @@ export * from './animate';
 export * from './avatar';
 export * from './breadcrumbs';
 export * from './charts';
+export * from './copy-clipboard-button';
 export * from './lazy-loader';
 export * from './page';
 export * from './router-link';
