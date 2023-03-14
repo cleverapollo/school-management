a84c28826c201db4072c1d939b3bff90872c17ac
cleverapollo <cleverapollo@gmail.com>
2023-03-14 09:29:50 +0000

    Add util function to merge strings based on boolean

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,4 @@ export * from './@types';
 export * from './components';
 export * from './hooks';
 export * from './providers';
+export * from './utils';
---
+++ packages/core/src/utils/index.ts
@@ -0,0 +1,1 @@
+export * from './get-classes-from-object';
