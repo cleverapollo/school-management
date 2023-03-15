723bc1475d929f8f71c8ba79074560ea00abe00b
cleverapollo <cleverapollo@gmail.com>
2023-03-15 14:09:06 +0000

    add school-building icon

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -33,6 +33,7 @@ export * from './person-tick';
 export * from './phone';
 export * from './pie-chart';
 export * from './save';
+export * from './school-building';
 export * from './search';
 export * from './send-mail';
 export * from './thumbs-up-clock';
