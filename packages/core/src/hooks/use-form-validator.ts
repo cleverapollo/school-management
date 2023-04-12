3a56b05d735dc552c8be844745d35171403d9379
cleverapollo <cleverapollo@gmail.com>
2023-04-12 13:18:39 +0100

    fix simplify the way to validate the mounted fields and nested fields

--- packages/core/src/hooks/use-form-validator.ts
+++ packages/core/src/hooks/use-form-validator.ts
@@ -149,29 +149,25 @@ export const useFormValidator = <TField extends FieldValues>(): {
     rules,
     resolver: (schema) => (fieldValues, _context, options) => {
       const keys = Object.keys(schema) as unknown as Array<keyof typeof schema>;
-      const mountedFields = keys
-        .filter((schemaKey) => options.fields[schemaKey])
-        .map((schemaKey) => ({ field: options.fields[schemaKey], schemaKey }));
 
-      const fieldsWithSchemaKey = mountedFields.flatMap(
-        ({ field, schemaKey }) => {
+      const fieldsWithSchemaKey = keys
+        .flatMap((schemaKey) => {
           const ruleFn = schema[schemaKey];
+          const field = options.fields[schemaKey];
 
           if (Array.isArray(field)) {
             return (field as ResolverOptions<TField>['fields'][]).flatMap(
               (itemField) =>
-                Object.keys(itemField)
-                  .filter((nestedKey) => itemField[nestedKey]?.mount)
-                  .map((nestedKey) => ({
-                    ...itemField[nestedKey],
-                    ruleFn: (ruleFn as NestedRules<TField>)?.[nestedKey],
-                  }))
+                Object.keys(itemField).map((nestedKey) => ({
+                  ...itemField[nestedKey],
+                  ruleFn: (ruleFn as NestedRules<TField>)?.[nestedKey],
+                }))
             );
           }
 
           return { ...field, ruleFn };
-        }
-      );
+        })
+        .filter(({ mount }) => mount);
 
       const errors = fieldsWithSchemaKey.reduce((fieldErrors, field) => {
         const [fieldName, index, itemField] = field.name.split('.');
