52f1810e930d5b467fadb85e0a38dcd22911b8b3
cleverapollo <cleverapollo@gmail.com>
2023-04-12 15:00:47 +0100

    Centralise get party name to be used in multiple areas of the calendar

---
+++ features/calendar/utils/get-party-name.ts
@@ -0,0 +1,41 @@
+import { Calendar_CalendarEventsQuery } from '@tyro/api';
+import { usePreferredNameLayout } from '@tyro/core';
+import { Attendee, PartyResource } from '../src/@types/calendar';
+
+export function getPartyName(
+  partyInfo: Attendee['partyInfo'] | PartyResource['partyInfo'],
+  getDisplayName: ReturnType<typeof usePreferredNameLayout>['displayName']
+) {
+  if (!partyInfo) return '';
+
+  switch (partyInfo.__typename) {
+    case 'GeneralGroup':
+      return partyInfo.name;
+    case 'Person':
+      return getDisplayName(partyInfo);
+    case 'Staff':
+    case 'Student':
+      return getDisplayName(partyInfo.person);
+    case 'SubjectGroup':
+      return partyInfo.name;
+    default:
+      return '';
+  }
+}
+
+export function getResourceName(
+  resource: NonNullable<
+    Calendar_CalendarEventsQuery['calendar_calendarEvents']
+  >['resources'][number],
+  getDisplayName: ReturnType<typeof usePreferredNameLayout>['displayName']
+) {
+  if (resource.__typename === 'PartyCalendarResource' && resource.partyInfo) {
+    getPartyName(resource.partyInfo, getDisplayName);
+  }
+
+  if (resource.__typename === 'RoomCalendarResource' && resource.room) {
+    return resource.room.name;
+  }
+
+  return '';
+}
