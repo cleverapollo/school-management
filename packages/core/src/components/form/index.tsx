e42f7bab8b82e2654705a8be6cbbe895211bb89d
cleverapollo <cleverapollo@gmail.com>
2023-05-05 17:31:58 +0100

    add RadioGroup component to core

--- packages/core/src/components/form/index.tsx
+++ packages/core/src/components/form/index.tsx
@@ -2,6 +2,7 @@ export * from './autocomplete';
 export * from './color-picker';
 export * from './date-picker';
 export * from './date-time-picker';
+export * from './radio-group';
 export * from './select';
 export * from './selection-list';
 export * from './switch';
