d1a5741e2e1828a09c017cd66f1d6ec3e09f8bef
cleverapollo <cleverapollo@gmail.com>
2023-05-12 15:19:51 +0100

    Updates to i18n

--- public/locales/en/index.ts
+++ public/locales/en/index.ts
@@ -1,29 +1,31 @@
-import attendance from './attendance.json';
-import assessments from './assessments.json';
-import authentication from './authentication.json';
-import common from './common.json';
-import navigation from './navigation.json';
-import settings from './settings.json';
-import userAccounts from './userAccounts.json';
-import groups from './groups.json';
-import people from './people.json';
 import admin from './admin.json';
+import assessments from './assessments.json';
+import attendance from './attendance.json';
+import authentication from './authentication.json';
 import calendar from './calendar.json';
+import common from './common.json';
+import groups from './groups.json';
 import mail from './mail.json';
+import navigation from './navigation.json';
+import people from './people.json';
+import settings from './settings.json';
+import sms from './sms.json';
 import substitution from './substitution.json';
+import userAccounts from './userAccounts.json';
 
 export const en = {
-  attendance,
+  admin,
   assessments,
+  attendance,
   authentication,
+  calendar,
   common,
+  groups,
+  mail,
   navigation,
   people,
   settings,
-  userAccounts,
-  groups,
-  admin,
-  calendar,
-  mail,
+  sms,
   substitution,
+  userAccounts,
 };
