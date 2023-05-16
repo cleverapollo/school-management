ec280829ba643588aa0a0da564f1145380001aa3
cleverapollo <cleverapollo@gmail.com>
2023-05-17 02:27:18 +0700

    add contact students

---
+++ features/people/src/api/contact/students.tsx
@@ -0,0 +1,60 @@
+import { useQuery } from '@tanstack/react-query';
+import { gqlClient, graphql, queryClient, Student } from '@tyro/api';
+import dayjs from 'dayjs';
+
+const contactsStudentsById = graphql(/* GraphQL */ `
+  query core_studentContacts_students($filter: StudentContactFilter!) {
+    core_studentContacts(filter: $filter) {
+      partyId
+      relationships {
+        studentPartyId
+        relationshipType
+        primaryContact
+        allowedToContact
+        includeInSms
+        ... on StudentContactRelationshipInfo {
+          student {
+            partyId
+            ... on Student {
+              person {
+                firstName
+                lastName
+                avatarUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+export const contactStudentsKeys = {
+  all: ['people', 'contact', 'students'] as const,
+  details: (contactId: number | undefined) =>
+    [...contactStudentsKeys.all, 'about', contactId] as const,
+};
+
+const contactStudentsQuery = (contactId: number | undefined) => ({
+  queryKey: contactStudentsKeys.details(contactId),
+  queryFn: async () =>
+    gqlClient.request(contactsStudentsById, {
+      filter: { studentContactPartyIds: [contactId ?? 0] },
+    }),
+});
+
+export function getContactStudents(contactId: number | undefined) {
+  return queryClient.fetchQuery(contactStudentsQuery(contactId));
+}
+
+export function useContactStudents(contactId: number | undefined) {
+  return useQuery({
+    ...contactStudentsQuery(contactId),
+    select: ({ core_studentContacts }) => {
+      const [studentContact] = core_studentContacts || [];
+      if (!studentContact?.relationships) return null;
+
+      return studentContact?.relationships;
+    },
+  });
+}
--- features/people/src/pages/contacts/profile/students.tsx
+++ features/people/src/pages/contacts/profile/students.tsx
@@ -1,7 +1,115 @@
 import { useParams } from 'react-router-dom';
+import {
+  GridOptions,
+  Table,
+  ICellRendererParams,
+  usePreferredNameLayout,
+  ReturnTypeDisplayNames,
+  ReturnTypeDisplayName,
+  StudyLevelSelectCellEditor,
+  TableAvatar,
+  useNumber,
+} from '@tyro/core';
+import { useMemo } from 'react';
+import { TFunction, useTranslation } from '@tyro/i18n';
+import set from 'lodash/set';
+import { useContactStudents } from '../../../api';
+
+type ReturnTypeFromUseContactStudents = NonNullable<
+  ReturnType<typeof useContactStudents>['data']
+>[number];
+
+const getContactStudentsColumns = (
+  t: TFunction<'common'[], undefined, 'common'[]>,
+  displayName: ReturnTypeDisplayName
+): GridOptions<ReturnTypeFromUseContactStudents>['columnDefs'] => [
+  {
+    field: 'name',
+    headerName: t('common:name'),
+    lockVisible: true,
+    cellRenderer: ({
+      data,
+    }: ICellRendererParams<ReturnTypeFromUseContactStudents>) => {
+      if (!data) return null;
+
+      const student = data?.student;
+
+      return (
+        <TableAvatar
+          name={displayName(student?.person) ?? ''}
+          to={`/people/students/${student?.partyId ?? ''}`}
+          avatarUrl={student?.person?.avatarUrl}
+        />
+      );
+    },
+    sort: 'asc',
+  },
+  {
+    field: 'classes',
+    headerName: 'Classes',
+    filter: true,
+    valueGetter: ({ data }) => '',
+    enableRowGroup: true,
+  },
+  {
+    field: 'relationship',
+    headerName: 'Relationship',
+    filter: true,
+    editable: true,
+    valueSetter: (params) => {
+      set(params.data ?? {}, 'irePP.level', params.newValue);
+      return true;
+    },
+    cellRenderer: ({
+      data,
+    }: ICellRendererParams<ReturnTypeFromUseContactStudents, any>) => null,
+    cellEditorSelector: StudyLevelSelectCellEditor(t),
+    enableRowGroup: true,
+  },
+  {
+    field: 'staff',
+    headerName: 'Is a Parent/Guardian',
+    valueGetter: ({ data }) => 'No',
+    enableRowGroup: true,
+  },
+  {
+    field: 'staff',
+    headerName: 'Has Pick-up Permission',
+    valueGetter: ({ data }) => 'No',
+    enableRowGroup: true,
+  },
+  {
+    field: 'staff',
+    headerName: 'Permission to View',
+    valueGetter: ({ data }) => 'No',
+    enableRowGroup: true,
+  },
+  {
+    field: 'staff',
+    headerName: 'Include in school communication',
+    valueGetter: ({ data }) => 'No',
+    enableRowGroup: true,
+  },
+];
 
 export default function ContactProfileStudentsPage() {
+  const { t } = useTranslation(['common', 'groups', 'people', 'mail']);
   const { id } = useParams();
+  const idNumber = useNumber(id);
+  const { displayName } = usePreferredNameLayout();
+  const { data: contactStudentsData } = useContactStudents(idNumber);
+  console.log('data', contactStudentsData);
 
-  return <div>Contact Profile Students Page {id}</div>;
+  const contactStudentColumns = useMemo(
+    () => getContactStudentsColumns(t, displayName),
+    [t, displayName]
+  );
+
+  return (
+    <Table
+      rowData={contactStudentsData ?? []}
+      columnDefs={contactStudentColumns}
+      getRowId={({ data }) => String(data?.studentPartyId)}
+    />
+  );
 }
