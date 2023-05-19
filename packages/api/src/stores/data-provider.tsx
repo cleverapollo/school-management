eb8fe500aa8cec9d88372ddff2c2986642e32d6c
cleverapollo <cleverapollo@gmail.com>
2023-05-19 20:11:37 +0100

    location and disabled added to rooms

--- packages/api/src/stores/data-provider.tsx
+++ packages/api/src/stores/data-provider.tsx
@@ -8,7 +8,7 @@ type DataProviderProps = {
   children: ReactNode;
 };
 
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://app.tyro-dev.com';
 const CUBEJS_TOKEN =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODQ2NzA2NjEsImlhdCI6MTY4NDQ3OTg2MX0.f8ASX8B6D5yqBOmk9go58H_dmyyK5oznGZ4GAKfl460';
 const cubejsApi = cubejs(CUBEJS_TOKEN, {
