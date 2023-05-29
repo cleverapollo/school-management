a872688650dd122ef7836c6bcafc349f4664671c
cleverapollo <cleverapollo@gmail.com>
2023-05-29 12:21:33 +0100

    Add sms cost from the backend

--- features/sms/src/api/keys.ts
+++ features/sms/src/api/keys.ts
@@ -1,4 +1,7 @@
+import { SmsCostFilter } from '@tyro/api';
+
 export const smsKeys = {
   all: ['sms'] as const,
   sent: () => [...smsKeys.all, 'sent'] as const,
+  cost: (filter: SmsCostFilter) => [...smsKeys.all, 'cost', filter] as const,
 };
---
+++ features/sms/src/api/sms-cost.tsx
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import { gqlClient, graphql, SmsCostFilter } from '@tyro/api';
+import { smsKeys } from './keys';
+
+const smsCost = graphql(/* GraphQL */ `
+  query communications_smsCost($filter: SmsCostFilter) {
+    communications_smsCost(filter: $filter) {
+      total
+    }
+  }
+`);
+
+const smsCostQuery = (filter: SmsCostFilter) => ({
+  queryKey: smsKeys.cost(filter),
+  queryFn: () => gqlClient.request(smsCost, { filter }),
+});
+
+export function useSmsCostPerMessage(filter: SmsCostFilter) {
+  return useQuery({
+    ...smsCostQuery(filter),
+    select: ({ communications_smsCost }) => communications_smsCost?.total ?? 0,
+    keepPreviousData: true,
+    staleTime: 1000 * 60 * 60,
+  });
+}
