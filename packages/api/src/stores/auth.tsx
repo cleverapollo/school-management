2cbea97cba85f48a1794d62fe857621aaeecb080
cleverapollo <cleverapollo@gmail.com>
2023-06-07 14:02:01 +0100

    Update msal to support prod + uat depending on hostname

--- packages/api/src/stores/auth.tsx
+++ packages/api/src/stores/auth.tsx
@@ -37,8 +37,6 @@ type AuthProviderProps = {
 function InnerAuthProvider({ children }: AuthProviderProps) {
   const { instance, accounts, inProgress } = useMsal();
   const account = useAccount(accounts[0] || {});
-  console.log('account');
-  console.log(account);
   const isAuthenticated = useIsAuthenticated();
   const [isTokenInitialized, setIsTokenInitialized] = useState(false);
 
--- packages/api/src/utils/msal-configs.ts
+++ packages/api/src/utils/msal-configs.ts
@@ -1,33 +1,66 @@
 import { AccountInfo, PublicClientApplication } from '@azure/msal-browser';
 import { setToken } from './jwt';
 
+const isProd = window.location.hostname === 'app.tyro.school';
+
 /**
  * Enter here the user flows and custom policies for your B2C application
  * To learn more about user flows, visit: https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-overview
  * To learn more about custom policies, visit: https://docs.microsoft.com/en-us/azure/active-directory-b2c/custom-policy-overview
  */
-export const b2cPolicies = {
-  names: {
-    signUpSignIn: 'B2C_1_signin',
-    forgotPassword: 'B2C_1_reset_password',
-    editProfile: 'B2C_1_edit_profile_v2',
-  },
-  authorities: {
-    signUpSignIn: {
-      authority:
-        'https://tyroschool.b2clogin.com/tyroschool.onmicrosoft.com/B2C_1_signin',
-    },
-    forgotPassword: {
-      authority:
-        'https://tyroschool.b2clogin.com/tyroschool.onmicrosoft.com/B2C_1_reset_password',
-    },
-    editProfile: {
-      authority:
-        'https://tyroschool.b2clogin.com/tyroschool.onmicrosoft.com/b2c_1_edit_profile',
-    },
-  },
-  authorityDomain: 'tyroschool.b2clogin.com',
-};
+const environmentMsalConfig = isProd
+  ? {
+      clientId: '99177d7e-e8ed-4c67-ab73-07eafc664492',
+      scope: 'https://tyroschool.onmicrosoft.com/tyro-api/graphql.use',
+      b2cPolicies: {
+        names: {
+          signUpSignIn: 'B2C_1_signin',
+          forgotPassword: 'B2C_1_reset_password',
+          editProfile: 'B2C_1_edit_profile_v2',
+        },
+        authorities: {
+          signUpSignIn: {
+            authority:
+              'https://tyroschool.b2clogin.com/tyroschool.onmicrosoft.com/B2C_1_signin',
+          },
+          forgotPassword: {
+            authority:
+              'https://tyroschool.b2clogin.com/tyroschool.onmicrosoft.com/B2C_1_reset_password',
+          },
+          editProfile: {
+            authority:
+              'https://tyroschool.b2clogin.com/tyroschool.onmicrosoft.com/b2c_1_edit_profile',
+          },
+        },
+        authorityDomain: 'tyroschool.b2clogin.com',
+      },
+    }
+  : {
+      clientId: '4dcc7808-8282-4b56-9518-ff8f306db255',
+      scope: 'https://tyrouat.onmicrosoft.com/tyro-api/graphql.use',
+      b2cPolicies: {
+        names: {
+          signUpSignIn: 'B2C_1_spa_login',
+          forgotPassword: 'B2C_1_spa_password_reset',
+          editProfile: 'B2C_1_edit_profile_v2',
+        },
+        authorities: {
+          signUpSignIn: {
+            authority:
+              'https://tyrouat.b2clogin.com/tyrouat.onmicrosoft.com/B2C_1_spa_login',
+          },
+          forgotPassword: {
+            authority:
+              'https://tyrouat.b2clogin.com/tyrouat.onmicrosoft.com/B2C_1_spa_password_reset',
+          },
+          editProfile: {
+            authority:
+              'https://tyrouat.b2clogin.com/tyrouat.onmicrosoft.com/b2c_1_edit_profile',
+          },
+        },
+        authorityDomain: 'tyrouat.b2clogin.com',
+      },
+    };
 
 /**
  * Configuration object to be passed to MSAL instance on creation.
@@ -36,9 +69,10 @@ export const b2cPolicies = {
  */
 export const msalConfig = {
   auth: {
-    clientId: '99177d7e-e8ed-4c67-ab73-07eafc664492', // This is the ONLY mandatory field that you need to supply.
-    authority: b2cPolicies.authorities.signUpSignIn.authority, // Use a sign-up/sign-in user-flow as a default authority
-    knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
+    clientId: environmentMsalConfig.clientId, // This is the ONLY mandatory field that you need to supply.
+    authority:
+      environmentMsalConfig.b2cPolicies.authorities.signUpSignIn.authority, // Use a sign-up/sign-in user-flow as a default authority
+    knownAuthorities: [environmentMsalConfig.b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
     redirectUri: `${window.location.origin}/auth/callback`, // Points to window.location.origin. You must register this URI on Azure Portal/App Registration.
     postLogoutRedirectUri: '/logout', // Indicates the page to navigate after logout.
     navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
@@ -62,11 +96,7 @@ export const msalInstance = new PublicClientApplication(msalConfig);
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-  scopes: [
-    'openid',
-    'profile',
-    'https://tyroschool.onmicrosoft.com/tyro-api/graphql.use',
-  ],
+  scopes: ['openid', 'profile', environmentMsalConfig.scope],
 };
 
 interface AcquireMsalTokenProps {
@@ -80,17 +110,13 @@ export async function acquireMsalToken(
   }
 ) {
   const activeAccount = account ?? instance.getActiveAccount();
-  console.log('activeAccount');
-  console.log(activeAccount);
   try {
     const response = await instance.acquireTokenSilent({
       ...loginRequest,
       account: activeAccount ?? undefined,
     });
-    console.log(response);
     instance.setActiveAccount(activeAccount);
     const token = response?.accessToken || null;
-    console.log(token);
     setToken(token);
     return token;
   } catch (error) {
