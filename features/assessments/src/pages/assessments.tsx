4b7bd584dbea78790335b56ad5ff3a16e38fbc46
cleverapollo <cleverapollo@gmail.com>
2023-06-19 19:26:30 +0700

    update feedback(sort by endDate)

--- features/assessments/src/pages/assessments.tsx
+++ features/assessments/src/pages/assessments.tsx
@@ -82,6 +82,9 @@ const getColumnDefs = (
     field: 'endDate',
     headerName: translate('common:endDate'),
     valueGetter: ({ data }) => (data ? dayjs(data.endDate).format('LL') : null),
+    sort: 'desc',
+    comparator: (dateA: string, dateB: string) =>
+      dayjs(dateA).unix() - dayjs(dateB).unix(),
   },
   {
     field: 'publish',
