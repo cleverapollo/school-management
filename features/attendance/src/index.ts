961cfc33e463928af6fedc88ff7dc58cb2d1cd28
cleverapollo <cleverapollo@gmail.com>
2023-06-20 20:15:13 +0100

    move temporary attendance codes to settings and hide attendance menu

--- features/attendance/src/index.ts
+++ features/attendance/src/index.ts
@@ -1,4 +1,9 @@
+import AttendanceCodes from './pages/codes';
+
 export * from './routes';
 
 export * from './api';
 export * from './components';
+
+// TODO: remove this export when we show the attendance menu
+export { AttendanceCodes };
