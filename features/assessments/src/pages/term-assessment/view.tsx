3e9503da5bd2b21dfce90d6e0f2ddcb2ec1ddff2
cleverapollo <cleverapollo@gmail.com>
2023-06-21 02:34:12 +0700

    Sort by teaching default.

--- features/assessments/src/pages/term-assessment/view.tsx
+++ features/assessments/src/pages/term-assessment/view.tsx
@@ -46,6 +46,7 @@ const getColumnDefs = (
     valueGetter: ({ data }) =>
       data?.subjectGroup.staff?.map((person) => displayName(person)).join(', '),
     enableRowGroup: true,
+    sort: 'asc',
   },
   {
     field: 'resultsTotal',
