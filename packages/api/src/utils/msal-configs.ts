c3851669f4b41f3fed378529ec3f5fe3f505b119
cleverapollo <cleverapollo@gmail.com>
2023-06-21 16:33:19 +0100

    Remove hard coding to point at prod

--- packages/api/src/utils/msal-configs.ts
+++ packages/api/src/utils/msal-configs.ts
@@ -1,8 +1,7 @@
 import { AccountInfo, PublicClientApplication } from '@azure/msal-browser';
 import { setToken } from './jwt';
 
-// const isProd = window.location.hostname === 'app.tyro.school';
-const isProd = true;
+const isProd = window.location.hostname === 'app.tyro.school';
 
 /**
  * Enter here the user flows and custom policies for your B2C application
