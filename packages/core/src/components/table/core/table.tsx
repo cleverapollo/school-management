012c6ebbdd34e76a07b9b13fc6db2dcd4a42ccd7
cleverapollo <cleverapollo@gmail.com>
2023-08-25 14:33:20 +0200

    Move AGGrid to the lastest version and change to module api to reduce load time

--- packages/core/src/components/table/core/table.tsx
+++ packages/core/src/components/table/core/table.tsx
@@ -2,24 +2,37 @@ import {
   useMemo,
   ForwardedRef,
   forwardRef,
-  MutableRefObject,
   useCallback,
   useState,
   useImperativeHandle,
   useRef,
 } from 'react';
-import { LicenseManager } from 'ag-grid-enterprise';
-import { AgGridReact, AgGridReactProps } from 'ag-grid-react';
-
-import 'ag-grid-community/styles/ag-grid.css';
-import { Box, BoxProps, Card, CardProps, Stack } from '@mui/material';
-
-import './styles.css';
 import {
+  ModuleRegistry,
   ColDef,
   ColumnRowGroupChangedEvent,
   FirstDataRenderedEvent,
-} from 'ag-grid-community';
+} from '@ag-grid-community/core';
+
+import { LicenseManager } from '@ag-grid-enterprise/core';
+
+import { AgGridReact, AgGridReactProps } from '@ag-grid-community/react';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { CsvExportModule } from '@ag-grid-community/csv-export';
+
+import { ClipboardModule } from '@ag-grid-enterprise/clipboard';
+import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { SetFilterModule } from '@ag-grid-enterprise/set-filter';
+
+import '@ag-grid-community/styles/ag-grid.css';
+import { Box, BoxProps, Card, CardProps, Stack } from '@mui/material';
+
+import './styles.css';
 import { useMeasure } from 'react-use';
 import { useMergeRefs } from '../../../hooks/use-merge-refs';
 import {
@@ -31,6 +44,19 @@ import { BulkEditSaveBar } from './bulk-edit-save-bar';
 import { SearchInput } from '../../search-input';
 import { TableLoadingOverlay } from './loading-overlay';
 
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  CsvExportModule,
+  ClipboardModule,
+  ExcelExportModule,
+  RangeSelectionModule,
+  MenuModule,
+  RowGroupingModule,
+  ColumnsToolPanelModule,
+  FiltersToolPanelModule,
+  SetFilterModule,
+]);
+
 export type {
   GridOptions,
   ICellRendererParams,
@@ -39,13 +65,13 @@ export type {
   ValueGetterParams,
   NewValueParams,
   ProcessCellForExportParams,
-} from 'ag-grid-community';
+} from '@ag-grid-community/core';
 export type {
   ReturnTypeUseEditableState as ReturnTypeTableUseEditableState,
   ValueSetterParams,
 } from '../hooks/use-editable-state';
 
-export type { AgGridReact } from 'ag-grid-react';
+export type { AgGridReact } from '@ag-grid-community/react';
 
 if (process.env.AG_GRID_KEY) {
   LicenseManager.setLicenseKey(process.env.AG_GRID_KEY);
@@ -127,9 +153,9 @@ function TableInner<T extends object>(
 
   useImperativeHandle(editingStateRef, () => editingUtils, [editingUtils]);
 
-  const colDefs = useMemo(
+  const colDefs = useMemo<ColDef<T>>(
     () => ({
-      ...defaultColDef,
+      ...(defaultColDef as ColDef<T>),
       ...props.defaultColDef,
     }),
     [props.defaultColDef]
