1fc9bcdddc1fa0061b73b25fc8c48e0b45ef8d82
cleverapollo <cleverapollo@gmail.com>
2023-09-04 12:01:22 +0100

    Add in ag grid status module

--- packages/core/src/components/table/core/table.tsx
+++ packages/core/src/components/table/core/table.tsx
@@ -23,6 +23,7 @@ import { CsvExportModule } from '@ag-grid-community/csv-export';
 import { ClipboardModule } from '@ag-grid-enterprise/clipboard';
 import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
 import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+import { StatusBarModule } from '@ag-grid-enterprise/status-bar';
 import { MenuModule } from '@ag-grid-enterprise/menu';
 import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
 import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
@@ -55,6 +56,7 @@ ModuleRegistry.registerModules([
   ColumnsToolPanelModule,
   FiltersToolPanelModule,
   SetFilterModule,
+  StatusBarModule,
 ]);
 
 export type {
