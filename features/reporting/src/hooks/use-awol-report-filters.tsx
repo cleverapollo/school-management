5cc60ee69179c81205c83d85b12d52147f0a7834
cleverapollo <cleverapollo@gmail.com>
2023-10-19 12:34:07 +0100

    Update filter to use current day as start date

--- features/reporting/src/hooks/use-awol-report-filters.tsx
+++ features/reporting/src/hooks/use-awol-report-filters.tsx
@@ -1,32 +1,28 @@
 import { useTranslation } from '@tyro/i18n';
-import { Reporting_TableFilterType, useAcademicNamespace } from '@tyro/api';
+import { Reporting_TableFilterType } from '@tyro/api';
 import dayjs from 'dayjs';
 
 export const useAwolReportFilters = () => {
   const { t } = useTranslation(['reports']);
-  const { activeAcademicNamespace } = useAcademicNamespace();
 
-  const defaultStartDate =
-    activeAcademicNamespace?.startDate || dayjs().format('YYYY-MM-DD');
-
-  const defaultEndDate = dayjs().format('YYYY-MM-DD');
+  const defaultDate = dayjs().format('YYYY-MM-DD');
 
   return [
     {
       id: 'from_date',
       inputType: Reporting_TableFilterType.Date,
       label: t('reports:from'),
-      defaultValue: defaultStartDate,
+      defaultValue: defaultDate,
       required: true,
-      minValue: defaultStartDate,
+      minValue: defaultDate,
     },
     {
       id: 'to_date',
       inputType: Reporting_TableFilterType.Date,
       label: t('reports:to'),
-      defaultValue: defaultEndDate,
+      defaultValue: defaultDate,
       required: true,
-      minValue: defaultStartDate,
+      minValue: defaultDate,
     },
   ];
 };
