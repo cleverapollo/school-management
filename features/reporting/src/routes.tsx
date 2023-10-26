ae670efe112958f7d5b11a83cb65177643c13033
cleverapollo <cleverapollo@gmail.com>
2023-10-26 11:41:08 +0200

    fix: fixed twice api call for report view page

--- features/reporting/src/routes.tsx
+++ features/reporting/src/routes.tsx
@@ -1,14 +1,7 @@
-import {
-  NavObjectFunction,
-  NavObjectType,
-  lazyWithRetry,
-  throw404Error,
-} from '@tyro/core';
+import { NavObjectFunction, NavObjectType, lazyWithRetry } from '@tyro/core';
 import { DocSearchIcon } from '@tyro/icons';
 import dayjs from 'dayjs';
-import { getCoreAcademicNamespace } from '@tyro/api';
 import { getReportsList } from './api/list';
-import { getRunReports } from './api/run-report';
 import { getAwolReportsQuery } from './api/awol-report';
 
 const ReportsListPage = lazyWithRetry(() => import('./pages'));
@@ -41,35 +34,11 @@ export const getRoutes: NavObjectFunction = (t) => [
             type: NavObjectType.NonMenuLink,
             path: ':id',
             element: <ReportContainer />,
-            loader: async ({ params }) => {
-              const { id = '' } = params;
-
-              if (!id) {
-                throw404Error();
-              }
-
-              return getRunReports({
-                topReportId: id,
-                filter: { reportId: id },
-              });
-            },
             children: [
               {
                 type: NavObjectType.NonMenuLink,
                 path: ':reportId',
                 element: <ReportPage />,
-                loader: async ({ params }) => {
-                  const { id = '', reportId = '' } = params;
-
-                  if (!id || !reportId) {
-                    throw404Error();
-                  }
-
-                  return getRunReports({
-                    topReportId: id,
-                    filter: { reportId },
-                  });
-                },
               },
             ],
           },
