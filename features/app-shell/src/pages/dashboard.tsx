eb20f7150dbc65ff1e777631135938c4dafd57de
cleverapollo <cleverapollo@gmail.com>
2023-11-10 21:45:59 +0000

    Updates for schedule widget

--- features/app-shell/src/pages/dashboard.tsx
+++ features/app-shell/src/pages/dashboard.tsx
@@ -2,9 +2,12 @@ import { Box, Container, Stack, Typography } from '@mui/material';
 import { Page } from '@tyro/core';
 import { useTranslation } from '@tyro/i18n';
 import { AWOLWidget } from '@tyro/reporting';
+import { TimetableWidget } from '@tyro/calendar';
+import { useUser } from '@tyro/api';
 
 export default function Dashboard() {
   const { t } = useTranslation(['navigation']);
+  const { activeProfile } = useUser();
 
   return (
     <Page title={t('navigation:general.dashboard')}>
@@ -12,12 +15,14 @@ export default function Dashboard() {
         <Typography variant="h3" component="h1" paragraph>
           {t('navigation:general.dashboard')}
         </Typography>
-        <Stack direction="row" spacing={2} flexWrap="wrap">
+        <Stack direction="row" spacing={2} useFlexGap flexWrap="wrap">
           <Box flex={1} minWidth="380px" maxWidth="420px">
             <AWOLWidget />
           </Box>
           <Box flex={1} minWidth="380px" maxWidth="420px" />
-          <Box flex={1} minWidth="380px" maxWidth="420px" />
+          <Box flex={1} minWidth="380px" maxWidth="420px">
+            <TimetableWidget partyId={activeProfile?.partyId ?? 0} />
+          </Box>
         </Stack>
       </Container>
     </Page>
--- features/calendar/src/components/common/timetable-widget.tsx
+++ features/calendar/src/components/common/timetable-widget.tsx
@@ -17,6 +17,7 @@ import {
   ChevronLeftIcon,
   ChevronRightIcon,
   ExternalLinkIcon,
+  FullScreenIcon,
 } from '@tyro/icons';
 import { useTranslation } from '@tyro/i18n';
 import { Link } from 'react-router-dom';
@@ -29,7 +30,11 @@ import {
   CalendarGridPeriodType,
   SchoolDayType,
 } from '@tyro/api';
-import { getClassesFromObject, usePreferredNameLayout } from '@tyro/core';
+import {
+  getClassesFromObject,
+  LoadingPlaceholderContainer,
+  usePreferredNameLayout,
+} from '@tyro/core';
 import { usePartyTimetable } from '../../api/timetable';
 import { useTimetableInPeriods } from '../../hooks/use-timetable-in-periods';
 
@@ -38,7 +43,7 @@ dayjs.extend(calendar);
 
 interface TimetableWidgetProps {
   partyId: number | undefined;
-  heading: string;
+  heading?: string;
   to?: string;
 }
 
@@ -68,11 +73,11 @@ function TimetableNonSchoolState({
 export function TimetableWidget({
   partyId,
   heading,
-  to,
+  to = '/calendar',
 }: TimetableWidgetProps) {
   const { t } = useTranslation(['common', 'assessments', 'calendar']);
   const [date, setDate] = useState(dayjs());
-  const { data } = usePartyTimetable({ partyId, date });
+  const { data, isLoading } = usePartyTimetable({ partyId, date });
   const dayInfo = useTimetableInPeriods(date, data);
   const { displayName } = usePreferredNameLayout();
 
@@ -86,216 +91,199 @@ export function TimetableWidget({
   });
 
   return (
-    <Card variant="outlined" sx={{ height: '100%', flex: 1, pb: 2 }}>
-      <CardHeader
-        component="h3"
-        title={heading}
-        {...(to && {
-          action: (
-            <IconButton component={Link} to={to}>
-              <ExternalLinkIcon sx={{ width: 20, height: 20 }} />
-            </IconButton>
-          ),
-        })}
-      />
+    <Card
+      variant="soft"
+      sx={{
+        flex: 1,
+      }}
+    >
       <Stack
         direction="row"
-        sx={{
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          px: 2,
-          py: 1,
-          borderBottom: '1px solid',
-          borderColor: 'divider',
-        }}
+        justifyContent="space-between"
+        alignItems="center"
+        pl={1}
+        mb={0.5}
       >
-        <IconButton
-          size="small"
-          color="primary"
-          onClick={() => {
-            setDate(date.subtract(1, 'day'));
-          }}
-        >
-          <ChevronLeftIcon />
-        </IconButton>
-        <Box sx={{ flex: 1, overflowX: 'hidden' }}>
-          <Typography
-            component="h4"
-            variant="subtitle2"
-            noWrap
-            sx={{ px: 2, textOverflow: 'ellipsis', textAlign: 'center' }}
-          >
-            {formattedDate}
-          </Typography>
-        </Box>
-        <IconButton
-          size="small"
-          color="primary"
-          onClick={() => {
-            setDate(date.add(1, 'day'));
-          }}
-        >
-          <ChevronRightIcon />
+        <Typography variant="h6" component="span">
+          {heading ?? t('calendar:inputLabels.schedule')}
+        </Typography>
+        <IconButton component={Link} to={to}>
+          <FullScreenIcon
+            sx={{ width: 20, height: 20, color: 'primary.main' }}
+          />
         </IconButton>
       </Stack>
-      {dayInfo.dayType !== SchoolDayType.PublicHoliday &&
-      dayInfo.dayType !== SchoolDayType.NonSchoolDay ? (
-        <TableContainer>
-          <Table
-            size="small"
-            sx={{
-              mt: 1,
-              px: 1.5,
-              borderCollapse: 'separate',
-              borderSpacing: 0,
-              '& td:first-of-type, & th:first-of-type': {
-                pl: 1.5,
-              },
-              '& td:last-of-type, & th:last-of-type': {
-                pr: 1.5,
-                textAlign: 'right',
-              },
-              '& th, & td:last-of-type': {
-                background: 'transparent',
-                color: 'text.primary',
-                fontWeight: 700,
-              },
-              '& th': {
-                py: 2,
-              },
-              '& td': {
-                color: 'text.secondary',
-              },
-              '& .current-period td': {
-                borderStyle: 'solid',
-                borderWidth: '1px 0',
-                borderColor: 'primary.main',
-              },
-              '& .break td': {
-                backgroundColor: 'indigo.50',
-              },
-              '& .break td:nth-of-type(2)': {
-                color: 'text.primary',
-              },
-              '& .current-period td:first-of-type, & .break td:first-of-type': {
-                borderTopLeftRadius: 17,
-                borderBottomLeftRadius: 17,
-              },
-              '& .current-period td:last-of-type, & .break td:last-of-type': {
-                borderTopRightRadius: 17,
-                borderBottomRightRadius: 17,
-              },
-              '& .current-period td:first-of-type': {
-                borderLeftWidth: 1,
-              },
-              '& .current-period td:last-of-type': {
-                borderRightWidth: 1,
-              },
-              '& .before-school td, & .after-school td': {
-                color: 'indigo.500',
-              },
-            }}
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell>{t('calendar:time')}</TableCell>
-                <TableCell>{t('calendar:lesson')}</TableCell>
-                <TableCell>{t('common:teacher')}</TableCell>
-                <TableCell>{t('calendar:room')}</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {dayInfo.periods?.map(
-                ({ startTime, endTime, type, event }, index) => {
-                  const isBreak = type === CalendarGridPeriodType.Break;
-                  const isBeforeSchoolStart = dayjs(startTime).isBefore(
-                    dayInfo.startTime
-                  );
-                  const isAfterSchoolEnd = dayjs(startTime).isAfter(
-                    dayInfo.endTime
-                  );
-                  const isLastEventBeforeSchoolStart =
-                    dayInfo.numberOfEventsBeforeSchool > 0 &&
-                    index === dayInfo.numberOfEventsBeforeSchool - 1;
-                  const isLastEventBeforeSchoolEnd =
-                    dayInfo.numberOfEventsAfterSchool > 0 &&
-                    index ===
-                      dayInfo.periods.length -
-                        dayInfo.numberOfEventsAfterSchool -
-                        1;
-                  const teacher = event?.attendees?.find(
-                    (attendee) =>
-                      attendee.type === CalendarEventAttendeeType.Organiser
-                  );
-                  const teacherName =
-                    teacher?.partyInfo?.__typename === 'Staff'
-                      ? displayName(teacher.partyInfo.person)
-                      : '-';
+      <Card
+        sx={{
+          minHeight: 128,
+        }}
+      >
+        <LoadingPlaceholderContainer isLoading={isLoading}>
+          {dayInfo.dayType !== SchoolDayType.PublicHoliday &&
+          dayInfo.dayType !== SchoolDayType.NonSchoolDay ? (
+            <TableContainer>
+              <Table
+                size="small"
+                sx={{
+                  px: 0.5,
+                  mb: 1.5,
+                  borderCollapse: 'separate',
+                  borderSpacing: 0,
+                  '& td:first-of-type, & th:first-of-type': {
+                    pl: 1.5,
+                  },
+                  '& td:last-of-type, & th:last-of-type': {
+                    pr: 1.5,
+                    textAlign: 'right',
+                  },
+                  '& th, & td:last-of-type': {
+                    background: 'transparent',
+                    color: 'text.primary',
+                    fontWeight: 700,
+                  },
+                  '& th': {
+                    py: 1,
+                  },
+                  '& td': {
+                    color: 'text.secondary',
+                  },
+                  '& .current-period td': {
+                    borderStyle: 'solid',
+                    borderWidth: '1px 0',
+                    borderColor: 'primary.main',
+                  },
+                  '& .break td': {
+                    backgroundColor: 'indigo.50',
+                  },
+                  '& .break td:nth-of-type(2)': {
+                    color: 'text.primary',
+                  },
+                  '& .current-period td:first-of-type, & .break td:first-of-type':
+                    {
+                      borderTopLeftRadius: 17,
+                      borderBottomLeftRadius: 17,
+                    },
+                  '& .current-period td:last-of-type, & .break td:last-of-type':
+                    {
+                      borderTopRightRadius: 17,
+                      borderBottomRightRadius: 17,
+                    },
+                  '& .current-period td:first-of-type': {
+                    borderLeftWidth: 1,
+                  },
+                  '& .current-period td:last-of-type': {
+                    borderRightWidth: 1,
+                  },
+                  '& .before-school td, & .after-school td': {
+                    color: 'indigo.500',
+                  },
+                }}
+              >
+                <TableHead>
+                  <TableRow>
+                    <TableCell>{t('calendar:time')}</TableCell>
+                    <TableCell>{t('calendar:lesson')}</TableCell>
+                    <TableCell>{t('common:teacher')}</TableCell>
+                    <TableCell>{t('calendar:room')}</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {dayInfo.periods?.map(
+                    ({ startTime, endTime, type, event }, index) => {
+                      const isBreak = type === CalendarGridPeriodType.Break;
+                      const isBeforeSchoolStart = dayjs(startTime).isBefore(
+                        dayInfo.startTime
+                      );
+                      const isAfterSchoolEnd = dayjs(startTime).isAfter(
+                        dayInfo.endTime
+                      );
+                      const isLastEventBeforeSchoolStart =
+                        dayInfo.numberOfEventsBeforeSchool > 0 &&
+                        index === dayInfo.numberOfEventsBeforeSchool - 1;
+                      const isLastEventBeforeSchoolEnd =
+                        dayInfo.numberOfEventsAfterSchool > 0 &&
+                        index ===
+                          dayInfo.periods.length -
+                            dayInfo.numberOfEventsAfterSchool -
+                            1;
+                      const teacher = event?.attendees?.find(
+                        (attendee) =>
+                          attendee.type === CalendarEventAttendeeType.Organiser
+                      );
+                      const teacherName =
+                        teacher?.partyInfo?.__typename === 'Staff'
+                          ? displayName(teacher.partyInfo.person)
+                          : '-';
 
-                  const subject = event?.attendees?.find(
-                    (attendee) =>
-                      attendee?.partyInfo?.__typename === 'SubjectGroup'
-                  );
-                  const subjectName =
-                    subject?.partyInfo?.__typename === 'SubjectGroup'
-                      ? subject.partyInfo.name
-                      : '-';
+                      const subject = event?.attendees?.find(
+                        (attendee) =>
+                          attendee?.partyInfo?.__typename === 'SubjectGroup'
+                      );
+                      const subjectName =
+                        subject?.partyInfo?.__typename === 'SubjectGroup'
+                          ? subject.partyInfo.name
+                          : '-';
 
-                  const roomNames =
-                    event?.rooms?.map((room) => room?.name).join(', ') ?? '-';
-                  const isCurrentClass =
-                    dayjs().isBefore(endTime) && dayjs().isAfter(startTime);
+                      const roomNames =
+                        event?.rooms?.map((room) => room?.name).join(', ') ??
+                        '-';
+                      const isCurrentClass =
+                        dayjs().isBefore(endTime) && dayjs().isAfter(startTime);
 
-                  return (
-                    <Fragment
-                      key={`${startTime ?? ''}-${event?.eventId ?? ''}`}
-                    >
-                      <TableRow
-                        className={getClassesFromObject({
-                          'current-period': isCurrentClass,
-                          break: isBreak,
-                          'before-school': isBeforeSchoolStart,
-                          'after-school': isAfterSchoolEnd,
-                          last:
-                            isLastEventBeforeSchoolStart ||
-                            isLastEventBeforeSchoolEnd,
-                        })}
-                      >
-                        <TableCell>{dayjs(startTime).format('H:mm')}</TableCell>
-                        <TableCell>
-                          {isBreak ? t('calendar:break') : subjectName}
-                        </TableCell>
-                        <TableCell>{teacherName}</TableCell>
-                        <TableCell>{roomNames}</TableCell>
-                      </TableRow>
-                      {(isLastEventBeforeSchoolEnd ||
-                        isLastEventBeforeSchoolStart) && (
-                        <TableRow
-                          key={`${startTime ?? ''}-${
-                            event?.eventId ?? ''
-                          }-divider`}
-                          aria-hidden
+                      return (
+                        <Fragment
+                          key={`${startTime ?? ''}-${event?.eventId ?? ''}`}
                         >
-                          <TableCell colSpan={4}>
-                            <Divider
-                              sx={{
-                                borderStyle: 'dashed',
-                                borderColor: 'divider',
-                              }}
-                            />
-                          </TableCell>
-                        </TableRow>
-                      )}
-                    </Fragment>
-                  );
-                }
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      ) : (
-        <TimetableNonSchoolState schoolDayType={dayInfo.dayType} />
-      )}
+                          <TableRow
+                            className={getClassesFromObject({
+                              'current-period': isCurrentClass,
+                              break: isBreak,
+                              'before-school': isBeforeSchoolStart,
+                              'after-school': isAfterSchoolEnd,
+                              last:
+                                isLastEventBeforeSchoolStart ||
+                                isLastEventBeforeSchoolEnd,
+                            })}
+                          >
+                            <TableCell>
+                              {dayjs(startTime).format('H:mm')}
+                            </TableCell>
+                            <TableCell>
+                              {isBreak ? t('calendar:break') : subjectName}
+                            </TableCell>
+                            <TableCell>{teacherName}</TableCell>
+                            <TableCell>{roomNames}</TableCell>
+                          </TableRow>
+                          {(isLastEventBeforeSchoolEnd ||
+                            isLastEventBeforeSchoolStart) && (
+                            <TableRow
+                              key={`${startTime ?? ''}-${
+                                event?.eventId ?? ''
+                              }-divider`}
+                              aria-hidden
+                            >
+                              <TableCell colSpan={4}>
+                                <Divider
+                                  sx={{
+                                    borderStyle: 'dashed',
+                                    borderColor: 'divider',
+                                  }}
+                                />
+                              </TableCell>
+                            </TableRow>
+                          )}
+                        </Fragment>
+                      );
+                    }
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          ) : (
+            <TimetableNonSchoolState schoolDayType={dayInfo.dayType} />
+          )}
+        </LoadingPlaceholderContainer>
+      </Card>
     </Card>
   );
 }
