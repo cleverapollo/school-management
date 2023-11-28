d9c2fce09420305b9e2b0f5b63a536520f93b378
cleverapollo <cleverapollo@gmail.com>
2023-11-28 11:00:42 +0000

    Refactor attendance and long-term leave components

--- features/attendance/src/components/attendance-for-cell.tsx
+++ features/attendance/src/components/attendance-for-cell.tsx
@@ -1,44 +1,44 @@
-import { FieldValues } from 'react-hook-form';
-import { usePreferredNameLayout } from '@tyro/core';
+import { PreferredNameFormat, usePreferredNameLayout } from '@tyro/core';
 import { Box, Popover, Stack, Typography, useTheme } from '@mui/material';
-import { useState } from 'react';
+import { useId, useState } from 'react';
 import { ReturnTypeFromUseBulkAttendance } from '../api/bulk-attendance/bulk-attendance';
 
 interface AttendanceForCellProps {
   data: ReturnTypeFromUseBulkAttendance;
 }
-export const AttendanceForCell = <TField extends FieldValues>({
-  data,
-}: AttendanceForCellProps) => {
+
+const NAME_MAX_LENGTH = 2;
+
+export const AttendanceForCell = ({ data }: AttendanceForCellProps) => {
+  const popoverId = useId();
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const theme = useTheme();
   const { displayName } = usePreferredNameLayout();
 
-  const attendanceForArr =
-    data?.parties?.map((party) => {
-      switch (party?.__typename) {
-        case 'Student':
-        case 'StudentContact':
-        case 'Staff':
-          return displayName(party?.person);
-        case 'SubjectGroup':
-          return party?.actualName;
-        default:
-          return party?.name ?? '-';
-      }
-    }) ?? [];
+  const partyNames =
+    data?.parties
+      ?.map((party) => {
+        switch (party?.__typename) {
+          case 'Student':
+          case 'StudentContact':
+          case 'Staff':
+            return displayName(party?.person, {
+              format: PreferredNameFormat.FirstnameSurname,
+            });
+          case 'SubjectGroup':
+            return party?.actualName;
+          default:
+            return party?.name ?? '-';
+        }
+      })
+      .sort((nameA, nameB) => nameA.localeCompare(nameB)) ?? [];
+  const isOverMaxLength = partyNames.length > NAME_MAX_LENGTH;
 
-  const splittedArr =
-    attendanceForArr
-      .join('^')
-      .slice(0, 31)
-      .split('^')
-      .filter((item) => item.replace('^', '')) ?? [];
-  const showLastItem =
-    splittedArr[splittedArr.length - 1] ===
-    attendanceForArr[splittedArr.length - 1];
-  const additionalItemsCount =
-    attendanceForArr.length - splittedArr.length + (showLastItem ? 0 : 1);
+  const shortName = isOverMaxLength
+    ? `${partyNames.slice(0, NAME_MAX_LENGTH).join(', ')} +${
+        partyNames.length - NAME_MAX_LENGTH
+      }`
+    : partyNames.join(', ');
 
   const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -48,55 +48,49 @@ export const AttendanceForCell = <TField extends FieldValues>({
     setAnchorEl(null);
   };
 
-  const open = Boolean(anchorEl);
+  const open = isOverMaxLength && Boolean(anchorEl);
 
   return (
     <>
       <Box
-        aria-owns={open ? 'mouse-over-popover' : undefined}
-        aria-haspopup="true"
+        component="span"
+        aria-owns={open ? popoverId : undefined}
+        aria-haspopup={isOverMaxLength}
         onMouseEnter={handlePopoverOpen}
         onMouseLeave={handlePopoverClose}
       >
-        <Typography>{`${(showLastItem
-          ? splittedArr
-          : splittedArr.slice(0, -1)
-        ).join(', ')}${
-          additionalItemsCount ? ` +${additionalItemsCount}` : ''
-        }`}</Typography>
+        {shortName}
       </Box>
-      <Popover
-        id="mouse-over-popover"
-        sx={{
-          pointerEvents: 'none',
-          // backgroundColor: theme.palette.common.white,
-          boxShadow: theme.customShadows.card,
-          color: theme.palette.text.primary,
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        {' '}
-        {attendanceForArr
-          .sort((nameA, nameB) => nameA.localeCompare(nameB))
-          .map((name) => (
-            <Stack direction="row" sx={{ p: 1 }}>
-              <Typography variant="body2" color="text.primary" sx={{ px: 1 }}>
+      {isOverMaxLength && (
+        <Popover
+          id={popoverId}
+          sx={{
+            pointerEvents: 'none',
+            boxShadow: theme.customShadows.card,
+            color: theme.palette.text.primary,
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+        >
+          <Stack sx={{ p: 1 }}>
+            {partyNames.map((name) => (
+              <Typography key={name} variant="body2" sx={{ px: 1 }}>
                 {name ?? ''}
               </Typography>
-            </Stack>
-          ))}
-      </Popover>
+            ))}
+          </Stack>
+        </Popover>
+      )}
     </>
   );
 };
