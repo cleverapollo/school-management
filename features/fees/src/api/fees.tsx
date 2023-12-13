2edf75fd1cf43cf3b89a185b78675655abbe64a0
cleverapollo <cleverapollo@gmail.com>
2023-12-13 15:20:43 -0600

    add discount list on the fee creation

--- features/fees/src/api/fees.tsx
+++ features/fees/src/api/fees.tsx
@@ -17,6 +17,10 @@ const fees = graphql(/* GraphQL */ `
       amount
       feeType
       absorbFees
+      discounts {
+        id
+        name
+      }
     }
   }
 `);
--- features/fees/src/components/fees/upsert-fee-modal.tsx
+++ features/fees/src/components/fees/upsert-fee-modal.tsx
@@ -1,5 +1,5 @@
 import { LoadingButton } from '@mui/lab';
-import { Button, Stack } from '@mui/material';
+import { Button, Chip, Stack } from '@mui/material';
 import {
   RHFTextField,
   Dialog,
@@ -10,14 +10,16 @@ import {
   RHFCheckbox,
   RHFRadioGroup,
   RHFDatePicker,
+  RHFAutocomplete,
 } from '@tyro/core';
 import { useTranslation } from '@tyro/i18n';
-import { FeeType } from '@tyro/api';
+import { FeeType, getColorBasedOnIndex } from '@tyro/api';
 import { useForm } from 'react-hook-form';
 import { useEffect } from 'react';
 import dayjs from 'dayjs';
 import { useSaveFee } from '../../api/save-fees';
 import { ReturnTypeFromUseFees } from '../../api/fees';
+import { ReturnTypeFromUseDiscounts, useDiscounts } from '../../api/discounts';
 
 type FeeFormState = {
   name: string;
@@ -25,6 +27,7 @@ type FeeFormState = {
   amount: number;
   feeType: FeeType;
   absorbFees: boolean;
+  discounts: ReturnTypeFromUseDiscounts[];
 };
 
 export type UpsertFeeModalProps = {
@@ -37,6 +40,7 @@ export function UpsertFeeModal({ open, value, onClose }: UpsertFeeModalProps) {
   const { t } = useTranslation(['common', 'fees']);
 
   const { mutateAsync: saveFee, isLoading } = useSaveFee();
+  const { data: discountsData = [] } = useDiscounts({});
 
   const { resolver, rules } = useFormValidator<FeeFormState>();
   const { control, handleSubmit, reset } = useForm<FeeFormState>({
@@ -48,13 +52,13 @@ export function UpsertFeeModal({ open, value, onClose }: UpsertFeeModalProps) {
     }),
   });
 
-  const onSubmit = handleSubmit(({ dueDate, amount, ...feeData }) =>
+  const onSubmit = handleSubmit(({ dueDate, amount, discounts, ...feeData }) =>
     saveFee(
       {
         ...feeData,
         amount: Number(amount),
         dueDate: dueDate.format('YYYY-MM-DD'),
-        discountIds: [],
+        discountIds: discounts.map(({ id }) => id),
       },
       {
         onSuccess: onClose,
@@ -63,8 +67,13 @@ export function UpsertFeeModal({ open, value, onClose }: UpsertFeeModalProps) {
   );
 
   useEffect(() => {
-    const { dueDate } = value || {};
-    reset({ ...value, dueDate: dueDate ? dayjs(dueDate) : undefined });
+    const { dueDate, discounts } = value || {};
+
+    reset({
+      ...value,
+      discounts: discounts || [],
+      dueDate: dueDate ? dayjs(dueDate) : undefined,
+    });
   }, [value]);
 
   return (
@@ -117,6 +126,26 @@ export function UpsertFeeModal({ open, value, onClose }: UpsertFeeModalProps) {
                 name: 'feeType',
                 control,
               }}
+            />
+
+            <RHFAutocomplete
+              multiple
+              label={t('fees:discounts')}
+              optionIdKey="id"
+              optionTextKey="name"
+              controlProps={{ name: 'discounts', control }}
+              options={discountsData}
+              renderTags={(tags, getTagProps) =>
+                tags.map((tag, index) => (
+                  <Chip
+                    {...getTagProps({ index })}
+                    size="small"
+                    variant="soft"
+                    color={getColorBasedOnIndex(tag.id)}
+                    label={tag.name}
+                  />
+                ))
+              }
             />
 
             <RHFCheckbox
