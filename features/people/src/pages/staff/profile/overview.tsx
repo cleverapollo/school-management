f835c4499a5bea7d2ee32ae4957edbdcbafd40eb
cleverapollo <cleverapollo@gmail.com>
2024-02-07 08:49:35 -0600

    remove overview tab under teacher profile

--- features/people/src/pages/staff/profile/overview.tsx
+++
@@ -1,8 +0,0 @@
-import { ProcessingDataPlaceholder } from '@tyro/core';
-import { useParams } from 'react-router-dom';
-
-export default function StaffProfileOverviewPage() {
-  const { id } = useParams();
-
-  return <ProcessingDataPlaceholder />;
-}
