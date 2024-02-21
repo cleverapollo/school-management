c48d490e579614c5c0328b8241cf7b18190f4952
cleverapollo <cleverapollo@gmail.com>
2024-02-21 21:29:31 +0000

    overdue removed as need to disable back end

--- features/app-shell/src/pages/dashboard.tsx
+++ features/app-shell/src/pages/dashboard.tsx
@@ -1,18 +1,14 @@
 import { Box, Container, Stack, Typography } from '@mui/material';
 import { MasonryGrid, Page } from '@tyro/core';
 import { useTranslation } from '@tyro/i18n';
-import {
-  AWOLWidget,
-  BehaviourWidget,
-  OverdueAttendanceWidget,
-} from '@tyro/reporting';
+import { AWOLWidget, BehaviourWidget } from '@tyro/reporting';
 import { TimetableWidget } from '@tyro/calendar';
 import { usePermissions, useUser } from '@tyro/api';
 
 export default function Dashboard() {
   const { t } = useTranslation(['navigation']);
   const { activeProfile } = useUser();
-  const { isStaffUserWithPermission, isTyroUser } = usePermissions();
+  const { isStaffUserWithPermission } = usePermissions();
 
   return (
     <Page title={t('navigation:general.dashboard')}>
@@ -30,8 +26,6 @@ export default function Dashboard() {
               showTeacher={false}
               partyId={activeProfile?.partyId ?? 0}
             />,
-            //  isStaffUserWithPermission('ps:1:attendance:write_attendance')
-            isTyroUser && <OverdueAttendanceWidget />,
           ]}
         />
       </Container>
